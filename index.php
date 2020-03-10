68fd03d5e938b19dc7c204842f42618d9ef80068
ICE <s5904022630062@email.kmutnb.ac.th>
2020-03-10 15:39:10 +0700

    add convert Celcius

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <h2>แปลงอุณหภูมิ</</h2>
 <p>
   <label>* Temperature:</label>
-  <input type="text" id="txttemp">
+  <input type="number" id="txttemp">
   <select id="typetemp">
   <option value="Fahrenheit">Fahrenheit</option>
   <option value="Celcius">Celcius</option>
@@ -18,30 +18,32 @@
 </div>
 <input type="submit" value="Submit" onclick="fucConverter();">
 
-
 <script>
 	function fucConverter(){
     var temp = document.getElementById("txttemp").value;
     var type = document.getElementById("typetemp");
     var typetemp = type.options[type.selectedIndex].value;
     var str_ ="";
-    var convCelcius,convRankine,convKelvin;
+    var convCelcius,convRankine,convKelvin,convFahrenheit;
     if(typetemp == "Fahrenheit"){
-     convCelcius = (temp-32)/1.8;
-     convRankine = (temp- 32)+ 491.67;
-     convKelvin =  ((temp -32)/1.8) +273.15;
-     
+     convCelcius = (temp - 32)/1.8;
+     convRankine = (temp - 32)+ 491.67;
+     convKelvin =  ((temp - 32)/1.8) +273.15;
+      str_ = convCelcius.toFixed(2) + "Celcius <p>" + convRankine.toFixed(2)+ "Rankine <p>" + convKelvin.toFixed(2) + "Rankine";
+    }else if (typetemp == "Celcius"){ 
+     convRankine = (temp * 1.8) + 491.67;
+     convKelvin =  temp + 273.15;
+     convFahrenheit = (temp * 1.8)+ 32;
+    str_ = convFahrenheit.toFixed(2) + "Fahrenheit <p>" + convRankine.toFixed(2)+ "Rankine <p>" + convKelvin + "Rankine";
     }
-    str_ = convCelcius.toFixed(2) + "Celcius <p>" + convRankine.toFixed(2)+ "Rankine <p>" + convKelvin.toFixed(2) + "Rankine";
+   
+     //ert(typetemp + str_);
      document.getElementById("txtResult").innerHTML= str_;
     
     }
 </script>
 
 </body>
-</html></div>
-</div>
-
-</body>
 </html>
+
 
