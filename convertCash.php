3308e140b34849a7d1b6b3194567024bd6ff3819
ICE <s5904022630062@email.kmutnb.ac.th>
2020-03-10 16:15:10 +0700

    edit

---
+++ convertCash.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<title>แปลงสกุลเงิน</title>
+<body>
+
+<h2>แปลงสกุลเงิน</</h2>
+<p>
+  <label>จำนวน:</label>
+  <input type="number" id="txtnum">
+  <label>บาท</label> 
+<p>
+<div id="txtResult">
+</div>
+<input type="submit" value="Submit" onclick="fucConverter();">
+
+<script>
+	function fucConverter(){
+    
+    var temp = document.getElementById("txtnum").value;
+    var res = parseInt(temp) * 38.01;
+    str_ = res + " วอน";    
+     document.getElementById("txtResult").innerHTML= str_;
+    
+    }
+</script>
+
+</body>
+</html>
