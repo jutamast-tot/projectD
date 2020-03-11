0a96a09fc17233702f3c091ad0700459a57df8b1
yunseree <jutamast@totbb.net>
2020-03-11 09:16:47 +0700

    convert to yen

--- convertCash.php
+++ convertCash.php
@@ -1,3 +1,16 @@
 <html>
+<title>แปลงเงินเยน</title>
+<body>
+<input type="text" name="THB" id="THB1"/>
 
+<div id="resulT"></div>
+<input type="submit" value="Submit" onclick="funcCon();">
+<script>
+function funcCon(){
+	var thai_ = document.getElementById("THB1").value;
+	var result = thai_ *0.303;
+	document.getElementById("resulT").innerHTML= result;
+}
+</script>
+</body>
 </html>
